aa91ed79ab675938a6d4a89f4188ad63f39e11aa
Carlosp256 <cparra2599@gmail.com>
2023-04-04 17:48:28 -0400

    intento de inicio de sesion

--- App1/App.js
+++ App1/App.js
@@ -1,39 +1,39 @@
-import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
 import React from 'react';
+import { View, Button, Alert } from 'react-native';
+import axios from 'axios';
 
-const App = () => {
-  const handlePress = () => {
-    Linking.openURL('https://open.spotify.com/playlist/0nqumhURrLHkUDC0TjLP5p?si=1fe3058a6dc64b3c');
+export default function TokenApp() {
+  const my_client_id = '5e42c81628ba42d480ca47a5dcc6691f';
+  const my_client_secret = '1b8da3986645461c931ff7cfe0519b92';
+
+  const handleAuthPress = async () => {
+    try {
+      // Realiza una solicitud para obtener un token de acceso
+      const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        params: {
+          grant_type: 'client_credentials'
+        },
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${my_client_id}:${my_client_secret}`).toString('base64')}`
+        }
+      });
+
+      // Extrae el token de acceso de la respuesta
+      const { access_token } = response.data;
+      console.log(`Token de acceso: ${access_token}`);
+
+      // Aquí puedes utilizar el token de acceso para realizar solicitudes a la API web de Spotify en nombre del usuario
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', 'Hubo un problema al iniciar sesión en Spotify.');
+    }
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={handlePress}
-        style={styles.button}>
-        <Text style={styles.buttonText}>babe</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Iniciar sesión en Spotify" onPress={handleAuthPress} />
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: '#1DB954',
-    padding: 10,
-    borderRadius: 20,
-  },
-  buttonText: {
-    fontSize: 50,
-    color: 'white',
-  },
-});
-
-export default App;
-
+}
